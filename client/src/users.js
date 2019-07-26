ea5a681cd3b79fdb5cb2bf39d11c6c4d0b7ec520
Kumar Yadav <n.kumar.yadav@accenture.com>
2019-07-26 12:13:18 +0530

    initial commit

---
+++ client/src/users.js
@@ -0,0 +1,21 @@
+export const jsonResponse = {
+  "data": {
+    "users": [
+      {
+          "username": "amy",
+          "password": "12345",
+          "isChatEnabled": true
+      },
+      {
+          "username": "nick",
+          "password": "12345",
+          "isChatEnabled": false
+      },
+      {
+          "username": "snowden",
+          "password": "12345",
+          "isChatEnabled": true
+      }
+    ]
+  }
+};
